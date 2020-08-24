f3a35f222429324c2a1b9958a74c687aa4fdf988
Pedro Piloto <Pedro_m_r_piloto@hotmail.com>
2020-08-24 12:55:36 +0100

    improvements

--- events-api/config/database.js
+++ events-api/config/database.js
@@ -1,15 +1,15 @@
-require("dotenv").config();
+require('dotenv').config();
 
 //Set up mongoose connection
-console.log("in db config");
-const mongoose = require("mongoose");
+console.log('in db config');
+const mongoose = require('mongoose');
 const nodeEnv = process.env.NODE_ENV;
-console.log("Node Env", nodeEnv);
+console.log('Node Env', nodeEnv);
 const mongoDB =
-  nodeEnv === "production" ? process.env.MONGODB_URI : process.env.MONGO_URL;
-console.log("Mongo URL", mongoDB);
-mongoose.set("useFindAndModify", false);
-mongoose.connect(mongoDB);
+  nodeEnv === 'production' ? process.env.MONGODB_URI : process.env.MONGO_URL;
+console.log('Mongo URL', mongoDB);
+mongoose.set('useFindAndModify', false);
+mongoose.connect(mongoDB, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
